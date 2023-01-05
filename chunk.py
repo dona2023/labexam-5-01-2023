07311b0cbfe275073212d0debebb3c12b13fc176
Dona m Thomas <95832329+dona2023@users.noreply.github.com>
2023-01-05 12:36:15 +0530

    Add files via upload

--- chunk.py
+++ chunk.py
@@ -1,7 +1,7 @@
 import nltk
-new="Open up your heart to Jesus-Let His Spirit enter in-And,forever,be your Saviour From life's struggles and their sins;Let Him true Companion"
+
+new="Hi Good morning im Dona M Thomas.I come from vechoochira. i complete my degree. now i studying mca. "
 print(new)
-from nltk.word_tokenzied
 new_token=nltk.word_tokenize(new)
 print(new_token)
 new_pos=nltk.pos_tag(new_token)
